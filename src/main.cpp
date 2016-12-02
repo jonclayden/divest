c8a75cc087b1bea642e69db0ea8817a33cbae4e9
Jon Clayden <code@clayden.org>
2016-12-02 14:07:57 +0000

    Adding R-callable interface function

--- src/main.cpp
+++ src/main.cpp
@@ -12,3 +12,31 @@ BEGIN_RCPP
     niftilib_register_all();
 END_RCPP
 }
+
+RcppExport SEXP readDirectory (SEXP path_)
+{
+BEGIN_RCPP
+    const std::string path = Rcpp::as<std::string>(path_);
+    
+    struct TDCMopts options;
+    options.isGz = false;
+    options.isFlipY = true;
+    options.isCreateBIDS = false;
+    options.isCreateText = false;
+    options.isTiltCorrect = false;
+    options.isRGBplanar = false;
+    options.isOnlySingleFile = false;
+    options.isForceStackSameSeries = false;
+    options.isCrop = false;
+    options.isVerbose = 0;
+    options.compressFlag = 0;
+    strcpy(options.indir, path.c_str());
+    options.imageList = (void *) Rcpp::List();
+    
+    int returnValue = nii_loadDir(&options);
+    if (returnValue == EXIT_SUCCESS)
+        return SEXP(options.imageList);
+    else
+        return R_NilValue;
+END_RCPP
+}
