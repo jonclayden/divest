65cc29e69e6d2d249e0eca0959416d584e7fdf08
Jon Clayden <code@clayden.org>
2017-01-11 15:53:47 +0000

    Enabling tilt correction (cf. PR #2)

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,7 @@ BEGIN_RCPP
     options.isFlipY = as<bool>(flipY_);
     options.isCreateBIDS = false;
     options.isCreateText = false;
-    options.isTiltCorrect = false;
+    options.isTiltCorrect = true;
     options.isRGBplanar = false;
     options.isOnlySingleFile = false;
     options.isForceStackSameSeries = false;
