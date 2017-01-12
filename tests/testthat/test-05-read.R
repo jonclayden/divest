7d3ea4d980fbdba98d8f7c55d645a139f03baa9d
Jon Clayden <code@clayden.org>
2017-01-12 13:39:36 +0000

    Adding further tests, for cropping and scanDicom()

--- tests/testthat/test-05-read.R
+++ tests/testthat/test-05-read.R
@@ -11,4 +11,11 @@ test_that("DICOM-reading code works", {
     
     origin <- RNifti::worldToVoxel(c(0,0,0), d[[i]])
     expect_equal(round(origin), c(-16,95,135))
+    
+    expect_output(d <- readDicom(path,interactive=FALSE,crop=TRUE), "Cropping")
+    i <- which(sapply(d,RNifti::ndim) == 3L)
+    expect_equal(dim(d[[i]]), c(2,224,170))
+    
+    expect_output(d <- scanDicom(path), "Found 4 DICOM")
+    expect_equal(d$repetitionTime, c(4100,11))
 })
