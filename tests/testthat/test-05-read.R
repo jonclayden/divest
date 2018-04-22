2caf05dd83d9771ec11231cd9da8ce5b319270a5
Jon Clayden <code@clayden.org>
2018-04-22 12:30:41 +0100

    Also testing content of compressed images

--- tests/testthat/test-05-read.R
+++ tests/testthat/test-05-read.R
@@ -27,12 +27,17 @@ test_that("DICOM-reading code works", {
 })
 
 test_that("we can read JPEG-encoded data sets", {
+    rawpath <- system.file("extdata", "raw", package="divest")
+    capture_output(r <- readDicom(rawpath, interactive=FALSE))
+    i <- which(sapply(r,RNifti::ndim) == 3L)
+    
     path <- system.file("extdata", "jpeg", package="divest")
     expect_output(d <- readDicom(path,interactive=FALSE), "Found 2 DICOM")
     
     expect_identical(length(d), 1L)
     expect_equal(dim(d[[1]]), c(2,224,256))
     expect_equal(attr(d[[1]],"flipAngle"), 15)
+    expect_equal(scale(as.array(d[[1]])), scale(as.array(r[[i]])), check.attributes=FALSE, tolerance=0.1)
     
     path <- system.file("extdata", "jpl", package="divest")
     expect_output(d <- readDicom(path,interactive=FALSE), "Found 2 DICOM")
@@ -40,6 +45,7 @@ test_that("we can read JPEG-encoded data sets", {
     expect_identical(length(d), 1L)
     expect_equal(dim(d[[1]]), c(2,224,256))
     expect_equal(attr(d[[1]],"flipAngle"), 15)
+    expect_equal(as.array(d[[1]]), as.array(r[[i]]))
 })
 
 test_that("DICOM file sorting works", {
