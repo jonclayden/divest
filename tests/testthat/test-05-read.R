4edb68a6b1eb80fc695ee3af5eebcc31a03dff93
Jon Clayden <code@clayden.org>
2019-04-24 17:03:21 +0100

    Adding test for multilevel file sorting (issue #3)

--- tests/testthat/test-05-read.R
+++ tests/testthat/test-05-read.R
@@ -72,15 +72,23 @@ test_that("we can read JPEG-encoded data sets", {
 })
 
 test_that("DICOM file sorting works", {
-    path <- system.file("extdata", "raw", package="divest")
-    temp <- tempdir()
-    file.copy(path, temp, recursive=TRUE)
-    path <- file.path(temp, "raw")
+    origPath <- system.file("extdata", "raw", package="divest")
+    tempPath <- tempdir()
+    file.copy(origPath, tempPath, recursive=TRUE)
+    tempPath <- file.path(tempPath, "raw")
     
-    expect_output(sortDicom(path), "Renamed 4 DICOM")
-    expect_equal(length(list.files(path)), 2L)
-    expect_true(all(c("T0_N_S8","T0_N_S9") %in% list.files(path)))
-    expect_output(readDicom(file.path(path,"T0_N_S8"),interactive=FALSE), "Found 2 DICOM")
+    expect_output(sortDicom(tempPath), "Renamed 4 DICOM")
+    expect_equal(length(list.files(tempPath)), 2L)
+    expect_true(all(c("T0_N_S8","T0_N_S9") %in% list.files(tempPath)))
+    expect_output(readDicom(file.path(tempPath,"T0_N_S8"),interactive=FALSE), "Found 2 DICOM")
     
-    unlink(path, recursive=TRUE)
+    unlink(tempPath, recursive=TRUE)
+    
+    dir.create(tempPath)
+    setwd(tempPath)
+    
+    expect_output(sortDicom(origPath, labelFormat="T%t/S%s/%4r.ima", nested=FALSE, keepUnsorted=TRUE), "Renamed 4 DICOM")
+    expect_true(all(c("S8","S9") %in% list.files(file.path(tempPath,"T0"))))
+    
+    unlink(tempPath, recursive=TRUE)
 })
